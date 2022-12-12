dc8949d0a55106a400bf5a5e72636627237c4d68
mohanad-ab <mohanadabuhamad78@gmail.com>
2022-12-12 21:51:47 +0200

    update

--- server/src/main/java/il/cshaifasweng/OCSFMediatorExample/server/SimpleServer.java
+++ server/src/main/java/il/cshaifasweng/OCSFMediatorExample/server/SimpleServer.java
@@ -22,7 +22,7 @@ public class SimpleServer extends AbstractServer {
 		String request = message.getMessage();
 		try {
 			//we got an empty message, so we will send back an error message with the error details.
-			if (request.isBlank()){
+			if (request.isBlank()) {
 				message.setMessage("Error! we got an empty message");
 				client.sendToClient(message);
 			}
@@ -30,37 +30,62 @@ public class SimpleServer extends AbstractServer {
 			// the IDs at data field in Message entity and send back to all subscribed clients a request to update
 			//their IDs text fields. An example of use of observer design pattern.
 			//message format: "change submitters IDs: 123456789, 987654321"
-			else if(request.startsWith("change submitters IDs:")){
+			else if (request.startsWith("change submitters IDs:")) {
 				message.setData(request.substring(23));
 				message.setMessage("update submitters IDs");
 				sendToAllClients(message);
 			}
-			//we got a request to add a new client as a subscriber.
-			else if (request.equals("add client")){
+			//we got a reque st to add a new client as a subscriber.
+			else if (request.equals("add client")) {
 				SubscribedClient connection = new SubscribedClient(client);
 				SubscribersList.add(connection);
 				message.setMessage("client added successfully");
 				client.sendToClient(message);
 			}
 			//we got a message from client requesting to echo Hello, so we will send back to client Hello world!
-			else if(request.startsWith("echo Hello")){
+			else if (request.startsWith("echo Hello")) {
 				message.setMessage("Hello World!");
 				client.sendToClient(message);
-			}
-			else if(request.startsWith("send Submitters IDs")){
+			} else if (request.startsWith("send Submitters IDs")) {
 				//add code here to send submitters IDs to client
-			}
-			else if (request.startsWith("send Submitters")){
+				message.setMessage("208842369, 318212248");
+				client.sendToClient(message);
+			} else if (request.startsWith("send Submitters")) {
 				//add code here to send submitters names to client
-			}
-			else if (request.equals("what day it is?")) {
+				message.setMessage("mohanad abuhamad, ahmad qais");
+				client.sendToClient(message);
+			} else if (request.equals("what day it is?")) {
 				//add code here to send the date to client
+				message.setMessage("8/12/2022");
+				client.sendToClient(message);
+			} else if (request.startsWith("add")) {
+				String str = message.getMessage();
+				String temp = "0";
+				int sum = 0;
+				for (int i = 0 ; i < str.length(); i++) {
+					char ch = str.charAt(i);
+					if (Character.isDigit(ch))
+						temp += ch;
+					else {
+						sum += Integer.parseInt(temp);
+						temp = "0";
+					}
+				}
+				 sum=sum + Integer.parseInt(temp);
+				String strint =Integer.toString(sum);
+
+				message.setMessage(strint);
+				client.sendToClient(message);
+
 			}
-			else if (request.startsWith("add")){
+
 				//add code here to sum 2 numbers received in the message and send result back to client
 				//(use substring method as shown above)
 				//message format: "add n+m"
-			}else{
+
+			else{
+				sendToAllClients(message);
+
 				//add code here to send received message to all clients.
 				//The string we received in the message is the message we will send back to all clients subscribed.
 				//Example:
